70094c398a4be1ffab4043a08075c02fbacde67f
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2022-09-12 21:55:28 +0800

    Create t.py

---
+++ tests/t.py
@@ -0,0 +1,27 @@
+import unittest
+from pyconn.client.lake.gcs import GCSClient
+import json
+import pathlib
+
+
+class GCSClientTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self) -> None:
+        credentials = json.load(open('../gcs-dev@yoov-dev.iam.gserviceaccount.com.json', 'r'))
+        self.client = GCSClient.from_kv(bucket='yoov-data-team-dev', credentials=credentials)
+        self.client.connect()
+
+    def test_upload_files(self):
+        self.client.execute('upload', 'pyproject-dev/pyconn-connect/test_gcs_lake.py', method='filename',
+                            filename=pathlib.Path.cwd() / 'test_gcs_lake.py')
+        import pandas as pd
+        df = pd.DataFrame([[123,123,123]])
+        self.client.execute('upload','pyproject-dev/pyconn-connect/test_file.csv',method='string',data=df.to_csv())
+
+    def test_download_files(self):
+        self.client.execute('download', 'pyproject-dev/pyconn-connect/test_gcs_lake.py', method='local', filename='t.py')
+
+if __name__ == '__main__':
+    unittest.main()
