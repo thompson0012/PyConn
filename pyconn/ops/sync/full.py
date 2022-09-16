7c389e1d229d40a1977000e9690d7dfcfeaa73b7
Thompson <51963680+thompson0012@users.noreply.github.com>
2022-09-16 14:46:35 +0800

    Update full.py

--- pyconn/ops/sync/full.py
+++ pyconn/ops/sync/full.py
@@ -1,8 +1,42 @@
 from pyconn.client.db.base import BaseDBClient
 from pyconn.ops.sync.base import BaseSyncDBClient
+from pyconn.utils.validator import validate_all_true
+from pyconn.utils.db_utils import substitute_sql
 
 
-class FullSyncClient(BaseSyncDBClient):
+class FullDBSyncClient(BaseSyncDBClient):
+    """
+    full database sync, required to have dropped / create statement for table manipulation
+    """
+
     def __init__(self, source_client=None, target_client=None):
-        super(FullSyncClient, self).__init__(source_client, target_client)
+        super(FullDBSyncClient, self).__init__(source_client, target_client)
+        self._drop_sql = None
+        self._create_sql = None
 
+    def register_drop_sql(self, sql):
+        self._drop_sql = sql
+        return
+
+    def register_create_sql(self, sql):
+        self._create_sql = sql
+        return
+
+    def sync(self, batch_size):
+        validate_all_true([self._extract_sql, self._load_sql, self._create_sql, self._drop_sql])
+        job_count = 0
+        q = self.run_extract_sql()
+        self.get_target_client().execute(self._drop_sql, keep_alive=True, commit=True)
+        self.get_target_client().execute(self._create_sql, keep_alive=True, commit=True)
+        while True:
+
+            print(job_count)
+            rows = q.fetchmany(batch_size)
+            if not bool(rows):
+                break
+            sub_sql = substitute_sql(self._load_sql,
+                                     rows)
+            self._target_client.execute(sub_sql, True, True)
+            job_count += 1
+
+        return
