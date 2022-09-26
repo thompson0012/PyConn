813d9f798f21e6ae4b5fd934153a6e907b197e7a
Thompson <51963680+thompson0012@users.noreply.github.com>
2022-09-26 15:46:24 +0800

    Update mysql.py

--- pyconn/client/db/mysql.py
+++ pyconn/client/db/mysql.py
@@ -1,14 +1,38 @@
 from pyconn.client.db.base import BaseDBClient
 import aiomysql
 import pymysql
-from typing import List
+from typing import List, Callable
 from pyconn.utils.db_utils import tuple_to_dict
-from pyconn.utils.validator import validate_opts_value
+from pyconn.utils.validator import validate_opts_value, validate_opts_type
 
 
 class MySQLClient(BaseDBClient):
     def __init__(self, db_params):
         super(MySQLClient, self).__init__(db_params)
+
+    def register_adapt(self, value_type, handler_func: Callable):
+        validate_opts_type(value_type, pymysql.FIELD_TYPE)
+        validate_opts_type(handler_func, Callable)
+
+        raise NotImplementedError
+
+    def register_conv(self, value_type, handler_func: Callable):
+        validate_opts_type(value_type, int)
+        validate_opts_type(handler_func, Callable)
+
+        conversions = self._db_params.get('conv', pymysql.converters.conversions)
+        conversions[value_type] = handler_func
+        self._db_params.update({'conv': conversions})
+        return
+
+    def init_default_conv(self):
+        self.register_conv(pymysql.FIELD_TYPE.DECIMAL, float)
+        self.register_conv(pymysql.FIELD_TYPE.DATE, str)
+        self.register_conv(pymysql.FIELD_TYPE.TIMESTAMP, str)
+        self.register_conv(pymysql.FIELD_TYPE.DATETIME, str)
+        self.register_conv(pymysql.FIELD_TYPE.TIME, str)
+
+        return
 
     def connect(self):
         conn = pymysql.connect(**self.get_db_params())
