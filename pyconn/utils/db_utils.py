dee6243b72d0625afb441970212c02963387352b
Thompson <51963680+thompson0012@users.noreply.github.com>
2022-09-26 15:45:44 +0800

    Update db_utils.py

--- pyconn/utils/db_utils.py
+++ pyconn/utils/db_utils.py
@@ -30,30 +30,6 @@ class ExtJsonEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-class SyncSqlResolver:
-    def __init__(self, regex=None, encoder=None):
-        self._regex = regex
-        self._encoder = encoder
-        self._regex_compiler = humre.compile(regex)
-
-    @classmethod
-    def from_default(cls):
-        BRACKET_CONTENT_EXP = humre.OPEN_BRACKET + humre.ANYTHING + humre.CLOSE_BRACKET
-        return cls(regex=BRACKET_CONTENT_EXP, encoder=ExtJsonEncoder)
-
-    def serialize(self, obj) -> "json string":
-        # return json.dumps(obj, cls=self._encoder, ensure_ascii=False)
-        return orjson.dumps(obj, option=orjson.OPT_UTC_Z).decode('utf-8')
-
-    def rewrite(self, json_str) -> str:
-        data_matched = self._regex_compiler.findall(json_str[1:-1])
-
-        def paren_first_last(obj):
-            return '(' + obj[1:-1] + ')'
-
-        return ','.join(list(map(lambda x: paren_first_last(x), data_matched)))
-
-
 class SqlBatchJoiner:
     def __init__(self):
         pass
