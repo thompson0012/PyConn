b3ceb62ecebad2d11a42fc5622e80cf8553e0e8e
Thompson <51963680+thompson0012@users.noreply.github.com>
2022-10-07 11:17:42 +0800

    Update mysql.py

--- pyconn/client/db/mysql.py
+++ pyconn/client/db/mysql.py
@@ -17,14 +17,16 @@ class MySQLClient(BaseDBClient):
         return self
 
     def show_table_schema(self, tbl_name):
-        data = self.execute(f'describe {tbl_name}', keep_alive=True).fetchall()
+        data = self.execute(f'describe {tbl_name}').fetchall()
         return map(lambda x: tuple_to_dict(x, ['field', 'type', 'null', 'key', 'default', 'extra']), data)
 
     def show_table_ddl(self, tbl_name):
-        data = self.execute(f'show create table {tbl_name}', keep_alive=True).fetchall()
+        data = self.execute(f'show create table {tbl_name}').fetchall()
         return map(lambda x: tuple_to_dict(x, ['table', 'sql']), data)
 
 
 class AsyncMySQLClient(MySQLClient):
     def __init__(self, db_params):
         super(AsyncMySQLClient, self).__init__(db_params)
+
+    
