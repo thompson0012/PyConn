6517c5f5bf9d58c54f863350ed81b11b01e86f01
Thompson <51963680+thompson0012@users.noreply.github.com>
2022-10-17 10:10:30 +0800

    remove

--- pyconn/enum/db_layer.py
+++
@@ -1,8 +0,0 @@
-class DBLayer:
-    ODS: str = 'ods'  # operational data store
-    DWD: str = 'dwd'  # data warehouse detail
-    DWS: str = 'dws'  # data warehouse service
-    DWM: str = 'dwm'  # data warehouse middle
-    DIM: str = 'dim'  # public
-    DW: str = 'dw'  # data warehouse
-    APP: str = 'app'  # application
